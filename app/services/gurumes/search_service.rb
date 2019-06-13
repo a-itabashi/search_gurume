63069947064115f36e53c8807e6116ae79e5d499
Aguru itabashi <itabashi.a@gmail.com>
2019-06-13 14:58:21 +0900

    api実装

---
+++ app/services/gurumes/search_service.rb
@@ -0,0 +1,15 @@
+module Gurumes
+  class SearchService
+    def initialize(area, food)
+      @area = area
+      @food = food
+      @hit = 20
+    end
+
+    def run
+      uri = URI.parse URI.encode("https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=#{ENV["GURUNAVI_API"]}&address=#{@area}&freeword=#{@food}&&hit_per_page=#{@hit}")
+      response = JSON.load(Net::HTTP.get(uri))
+      response["rest"]
+    end
+  end
+end
